f370ef75845fe1cf2b98c03126e6bbec1329a66e
Matheus <matheuslippitec@gmail.com>
2023-10-29 15:16:41 -0300

    Testing commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace DapperCrud
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=Company;User ID=sa;Password=1q2w3e4r@#$";
+
+
+            using (var connection = new SqlConnection())
+            {
+
+            }
+
+
+
+        }
+    }
+}
